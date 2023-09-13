f7105dd199bc29c55abd15f69c1dbc96cea29189
Bolotin Alexander <bolotin@giox.ru>
2023-09-13 23:23:58 +0300

    Masked input added

--- components/common/inputTel/inputTel.tsx
+++ components/common/inputTel/inputTel.tsx
@@ -1,5 +1,6 @@
 import React, { InputHTMLAttributes } from "react";
 import s from "./inputTel.module.scss";
+import InputMask from "react-input-mask";
 
 type Props = {
   id: string;
@@ -10,13 +11,23 @@ const InputTel: React.FC<Props> = ({ id, placeholder, ...props }) => {
   const { type, ...otherProps } = props;
   return (
     <div className={s.wrapper}>
-        <input
-          className={s.text}
-          type="text"
-          {...otherProps}
-          id={id}
-          placeholder={placeholder}
-        />
+      <InputMask
+        mask="+7 999 999 99 99"
+        value={props.value}
+        onChange={props.onChange}
+      >
+        {() => {
+          return (
+            <input
+              className={s.text}
+              type="text"
+              {...otherProps}
+              id={id}
+              placeholder={placeholder}
+            />
+          );
+        }}
+      </InputMask>
       <label className={s.label} htmlFor={id}>
         {placeholder}
       </label>
